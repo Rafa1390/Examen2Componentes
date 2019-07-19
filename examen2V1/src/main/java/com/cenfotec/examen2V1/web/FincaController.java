6909d2bfb9ef6d988ef45d7244f3ea15eb8b3c5a
Rafael Briceño <rbricenoa@ucenfotec.ac.cr>
2019-07-19 11:07:10 -0600

    Se añade la funcionalidad de Post a FincaController

--- examen2V1/src/main/java/com/cenfotec/examen2V1/web/FincaController.java
+++ examen2V1/src/main/java/com/cenfotec/examen2V1/web/FincaController.java
@@ -1,16 +1,31 @@
 package com.cenfotec.examen2V1.web;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.cenfotec.examen2V1.domain.Finca;
+import com.cenfotec.examen2V1.repository.FincaRepository;
 
 @Controller
 public class FincaController {
+	@Autowired
+	FincaRepository repo;
+	
 	@GetMapping("/finca")
 	public String createFincaView(Model model) {
 		model.addAttribute("finca", new Finca());
         return "finca";
+	}
+	
+	@PostMapping("/finca")
+	public String createFinca(@Valid Finca finca, BindingResult result) {
+		repo.save(finca);
+		return "finca";
 	}
 }
