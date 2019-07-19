2490e300580cdadb1a892d70f3295635b8286a58
Rafael Briceño <rbricenoa@ucenfotec.ac.cr>
2019-07-19 10:12:09 -0600

    Se genera el proyecto nuevo

---
+++ examen2V1/src/test/java/com/cenfotec/examen2V1/Examen2V1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cenfotec.examen2V1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Examen2V1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
