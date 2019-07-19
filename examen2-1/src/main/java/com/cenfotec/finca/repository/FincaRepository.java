4cea1b7739fb4b6fcb4fb204688487119219083c
Rafael Briceño <rbricenoa@ucenfotec.ac.cr>
2019-07-19 09:59:40 -0600

    Se eliminó todo

--- examen2-1/src/main/java/com/cenfotec/finca/repository/FincaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cenfotec.finca.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cenfotec.finca.domain.Finca;
-
-public interface FincaRepository extends JpaRepository<Finca, Long>{
-
-}
