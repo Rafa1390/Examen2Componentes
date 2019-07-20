a2e262d2d0cba046494178fbf20336d587aff81c
Rafael Briceño <rbricenoa@ucenfotec.ac.cr>
2019-07-19 19:51:10 -0600

    Se listan fincas y se comunican paginas de la app

--- examen2V1/src/main/java/com/cenfotec/examen2V1/web/FincaController.java
+++ examen2V1/src/main/java/com/cenfotec/examen2V1/web/FincaController.java
@@ -5,9 +5,13 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.cenfotec.examen2V1.domain.Finca;
 import com.cenfotec.examen2V1.repository.FincaRepository;
@@ -23,9 +27,15 @@ public class FincaController {
         return "finca";
 	}
 	
+	@RequestMapping(value="", method = RequestMethod.GET)
+    public String listaFincas(ModelMap mp){
+        mp.put("fincas", repo.findAll());
+        return "finca_lista";
+    }
+	
 	@PostMapping("/finca")
-	public String createFinca(@Valid Finca finca, BindingResult result) {
+	public ModelAndView createFinca(@Valid Finca finca, BindingResult result) {
 		repo.save(finca);
-		return "finca";
+		return new ModelAndView("redirect:/");
 	}
 }
